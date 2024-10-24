b4eb11fc511f68621b0a7038ba66e2dcec3fbb67
StylingOnZ <theanhaptx@gmail.com>
2024-10-24 16:41:18 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace dgv
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            dataGridView1 = new DataGridView();
+            Mã = new DataGridViewTextBoxColumn();
+            Column1 = new DataGridViewTextBoxColumn();
+            Lớp = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(228, 172);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Mã sinh viên";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(228, 212);
+            label2.Name = "label2";
+            label2.Size = new Size(78, 15);
+            label2.TabIndex = 2;
+            label2.Text = " Tên sinh viên";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(228, 239);
+            label3.Name = "label3";
+            label3.Size = new Size(30, 15);
+            label3.TabIndex = 3;
+            label3.Text = " Lớp";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(308, 172);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(263, 23);
+            textBox1.TabIndex = 4;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(308, 204);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(263, 23);
+            textBox2.TabIndex = 5;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(308, 239);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(263, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(228, 278);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 7;
+            button1.Text = " Thêm";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(369, 278);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 8;
+            button2.Text = " Sửa";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(496, 278);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 9;
+            button3.Text = " Xóa";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Mã, Column1, Lớp });
+            dataGridView1.Location = new Point(228, 27);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(343, 109);
+            dataGridView1.TabIndex = 10;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // Mã
+            // 
+            Mã.HeaderText = "Mã sinh viên";
+            Mã.Name = "Mã";
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "Tên sinh viên";
+            Column1.Name = "Column1";
+            // 
+            // Lớp
+            // 
+            Lớp.HeaderText = "Lớp";
+            Lớp.Name = "Lớp";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Mã;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Lớp;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace StudentManagement
+{
+    public partial class Form1 : Form
+    {
+        private List<Student> students; // Danh sách sinh viên
+
+        public Form1()
+        {
+            InitializeComponent();
+            students = new List<Student>(); // Khởi tạo danh sách sinh viên
+            InitializeDataGridView(); // Thiết lập DataGridView
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshDataGridView(); // Làm mới DataGridView khi form tải
+        }
+
+        private void InitializeDataGridView()
+        {
+            dataGridViewStudents.Columns.Add("studentID", "Mã Sinh Viên");
+            dataGridViewStudents.Columns.Add("name", "Tên Sinh Viên");
+            dataGridViewStudents.Columns.Add("class", "Lớp Học");
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e) // Nút "Thêm"
+        {
+            var student = new Student
+            {
+                StudentID = textBoxStudentID.Text,
+                Name = textBoxName.Text,
+                Class = textBoxClass.Text
+            };
+            students.Add(student); // Thêm sinh viên vào danh sách
+            RefreshDataGridView(); // Làm mới DataGridView
+            ClearInputFields(); // Xóa các trường nhập liệu
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e) // Nút "Sửa"
+        {
+            if (dataGridViewStudents.SelectedRows.Count > 0)
+            {
+                var selectedRow = dataGridViewStudents.SelectedRows[0];
+                selectedRow.Cells["studentID"].Value = textBoxStudentID.Text;
+                selectedRow.Cells["name"].Value = textBoxName.Text;
+                selectedRow.Cells["class"].Value = textBoxClass.Text;
+
+                var index = students.FindIndex(s => s.StudentID == selectedRow.Cells["studentID"].Value.ToString());
+                students[index].Name = textBoxName.Text;
+                students[index].Class = textBoxClass.Text;
+
+                RefreshDataGridView(); // Làm mới DataGridView
+                ClearInputFields(); // Xóa các trường nhập liệu
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một sinh viên để sửa.");
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e) // Nút "Xóa"
+        {
+            if (dataGridViewStudents.SelectedRows.Count > 0)
+            {
+                var selectedRow = dataGridViewStudents.SelectedRows[0];
+                students.RemoveAll(s => s.StudentID == selectedRow.Cells["studentID"].Value.ToString());
+                RefreshDataGridView(); // Làm mới DataGridView
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một sinh viên để xóa.");
+            }
+        }
+
+        private void RefreshDataGridView()
+        {
+            dataGridViewStudents.Rows.Clear(); // Xóa tất cả hàng hiện tại
+            foreach (var student in students)
+            {
+                dataGridViewStudents.Rows.Add(student.StudentID, student.Name, student.Class); // Thêm hàng mới
+            }
+        }
+
+        private void ClearInputFields()
+        {
+            textBoxStudentID.Clear();
+            textBoxName.Clear();
+            textBoxClass.Clear();
+        }
+    }
+
+    public class Student
+    {
+        public string StudentID { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+    }
+}
